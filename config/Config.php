c6ca605ff7cffa26d2ee7b8da8cf48a35483eb5d
dahye03 <84061645+dahye03@users.noreply.github.com>
2024-07-31 17:37:46 +0900

    연결테스트

--- config/Config.php
+++ config/Config.php
@@ -2,7 +2,7 @@
 
 class Config
 {
-    const DB_HOST = '112.175.85.214';
+    const DB_HOST = 'localhost';
     const DB_PORT = '3306';
     const DB_USER = 'hompro';
     const DB_PASS = 'dlehddms1014!';
